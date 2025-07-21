f22d81d5811524c0d1c9f2e683ef8f2685ade66a
Aldrich Reboja <aktreboja@gmail.com>
2025-07-21 01:49:58 -0700

    Auth0 implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,27 @@
 import { Button } from '@/components/ui/button';
-import { headers } from 'next/headers';
+import { auth0 } from '@/lib/auth0';
 import Link from 'next/link';
-export default async function Home() {
-  // Get the host from headers for server-side fetch
-  const headersList = await headers();
-  const host = headersList.get('host');
-  const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+import { useUser } from '@auth0/nextjs-auth0';
 
-  const res = await fetch(`${protocol}://${host}/api/me`, {
-    cache: 'no-store',
-  });
-  const data = await res.json();
+export default async function Home() {
+  const session = await auth0.getSession();
+  const user = session?.user;
+
+  console.log(user);
 
   return (
     <div>
       <main className="flex flex-col gap-4 items-center justify-center h-screen">
         <h1 className="text-4xl font-bold">NightOwl</h1>
-        <Button
-          className="cursor-pointer"
-          variant="outline"
-        >
-          <Link href="/login">Get Started</Link>
+        <Button className="cursor-pointer" variant="outline">
+          <Link href="/auth/login">Get Started</Link>
         </Button>
+        {user && <p>Welcome {user.name}</p>}
+        {user && (
+          <Button className="cursor-pointer" variant="outline">
+            <Link href="/auth/logout">Logout</Link>
+          </Button>
+        )}
       </main>
     </div>
   );
